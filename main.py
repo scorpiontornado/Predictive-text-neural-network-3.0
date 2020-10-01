56663958f9098046a510b73fca502e2c0fc772d2
scorpiontornado <langfordn@icloud.com>
2020-10-01 14:55:58 +1000

    Added model that I trained on Kaggle (https://www.kaggle.com/scorpiontornado/predictive-text-neural-network)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
-import cv2 as cv
+#import cv2 as cv
 
 import pickle
 
@@ -16,6 +16,9 @@ import random
 # pygame project imports
 import string
 import pygame
+
+# import to load the neural network that I made on Kaggle (see line 1)
+from tensorflow import keras
 
 
 class Node:
@@ -110,7 +113,7 @@ class Node:
 
 
 class Digit:
-    def __init__(self, x, y, width, colour, surface, data, values, mapping, height=None, border_width=5):
+    def __init__(self, x, y, width, colour, surface, data, mapping, height=None, border_width=5):
         self.x = x
         self.y = y
         self.width = width
@@ -132,24 +135,29 @@ class Digit:
 
         plt.imshow(self.cur_digit, cmap="Greys")
         plt.axis("off")
-        plt.title(chr(self.mapping[int(self.predict())]), y=-0.15, color="green")
+        plt.title(chr(self.mapping[int(self.predict())]),
+                  y=-0.15, color="green")
         plt.savefig("images/transposed_digit.png")
 
-        self.cur_dig_img = pygame.image.load('images/transposed_digit.png').convert()
+        self.cur_dig_img = pygame.image.load(
+            'images/transposed_digit.png').convert()
         self.rect = self.cur_dig_img.get_rect()
-        self.cur_dig_img = pygame.transform.scale(self.cur_dig_img, (self.width-self.border_width*2, int((self.rect[3]/self.rect[2])*self.width)-self.border_width*2))
+        self.cur_dig_img = pygame.transform.scale(
+            self.cur_dig_img, (self.width-self.border_width*2, int((self.rect[3]/self.rect[2])*self.width)-self.border_width*2))
         if not self.height:
             # ternary operator needs an else statement, so I used a single line if statement
             self.height = int((self.rect[3]/self.rect[2])*self.width)
 
         self.rect = self.cur_dig_img.get_rect()
-        self.rect = self.rect.move(int(self.x+self.border_width), int(self.y+int((self.height-self.rect[3])/2))) if int((self.height-self.rect[3])/2) > self.border_width else self.rect.move(int(self.x+self.border_width), int(self.y+self.border_width))  # a = b + 2c, c = (a-b)/2
+        self.rect = self.rect.move(int(self.x+self.border_width), int(self.y+int((self.height-self.rect[3])/2))) if int(
+            (self.height-self.rect[3])/2) > self.border_width else self.rect.move(int(self.x+self.border_width), int(self.y+self.border_width))  # a = b + 2c, c = (a-b)/2
 
     def predict(self):
         return self.values[self.index]
 
     def draw(self):
-        pygame.draw.rect(self.surface, self.colour, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(self.surface, self.colour,
+                         (self.x, self.y, self.width, self.height))
         self.surface.blit(self.cur_dig_img, self.rect)
 
 
@@ -174,24 +182,32 @@ class Button:
 
     def draw(self):
         # vertically short rect
-        pygame.draw.rect(self.surface, self.colour, (self.rect[0], self.rect[1]+self.r, self.rect[2], self.rect[3]-2*self.r))
-        pygame.draw.rect(self.surface, self.colour, (self.rect[0]+self.r, self.rect[1], self.rect[2]-2*self.r, self.rect[3]))  # vertically tall rect
+        pygame.draw.rect(self.surface, self.colour,
+                         (self.rect[0], self.rect[1]+self.r, self.rect[2], self.rect[3]-2*self.r))
+        pygame.draw.rect(self.surface, self.colour, (
+            self.rect[0]+self.r, self.rect[1], self.rect[2]-2*self.r, self.rect[3]))  # vertically tall rect
 
-        pygame.draw.circle(self.surface, self.colour, (self.x+self.r, self.y+self.r), self.r)  # top left
-        pygame.draw.circle(self.surface, self.colour, (self.x + self.width-self.r, self.y+self.r), self.r)  # top right
-        pygame.draw.circle(self.surface, self.colour, (self.x+self.width - self.r, self.y+self.height-self.r), self.r)  # bottom right
-        pygame.draw.circle(self.surface, self.colour, (self.x+self.r, self.y+self.height-self.r), self.r)  # bottom left
+        pygame.draw.circle(self.surface, self.colour,
+                           (self.x+self.r, self.y+self.r), self.r)  # top left
+        pygame.draw.circle(self.surface, self.colour, (self.x +
+                                                       self.width-self.r, self.y+self.r), self.r)  # top right
+        pygame.draw.circle(self.surface, self.colour, (self.x+self.width -
+                                                       self.r, self.y+self.height-self.r), self.r)  # bottom right
+        pygame.draw.circle(self.surface, self.colour, (self.x+self.r,
+                                                       self.y+self.height-self.r), self.r)  # bottom left
 
         self.text = self.font.render(self.string, True, self.text_colour)
 
         try:
-            self.text = pygame.transform.scale(self.text, (int(self.width*0.8), int(self.height*(-1/500*self.text.get_rect()[2]+0.9))))
+            self.text = pygame.transform.scale(self.text, (int(
+                self.width*0.8), int(self.height*(-1/500*self.text.get_rect()[2]+0.9))))
         except ValueError:
             print("ValueError: Cannot scale to negative size")
 
         self.text_rect = self.text.get_rect()
 
-        self.text_rect.center = (self.x + self.x+self.width)//2, (self.y + self.y+self.height)//2
+        self.text_rect.center = (
+            self.x + self.x+self.width)//2, (self.y + self.y+self.height)//2
 
         self.surface.blit(self.text, self.text_rect)
 
@@ -200,8 +216,11 @@ class PredictiveText:
     pass
 
 
-def append_letter(l, w):
-    w.append(l)
+def append_letter(i, w, model, y, mapping):
+    # w.append(l)
+    #prediction = model.predict(cur_digit)
+    prediction = chr(mapping[int(y[i])])
+    w.append(prediction)
 
 
 def append_word(w, s):
@@ -222,13 +241,20 @@ def load_map(map_path):
 
 
 def load_data(data_path):
-    df_test = pd.read_csv(
-        data_path, names=["label"]+["pixel"+str(x) for x in range(784)])
+    # df_test = pd.read_csv(data_path, names=["label"]+["pixel"+str(x) for x in range(784)])
 
-    test_x = np.asarray(df_test.iloc[:, 1:]).reshape([-1, 28, 28, 1])
-    test_y = np.asarray(df_test.iloc[:, 0]).reshape([-1, 1])
+    # test_x = np.asarray(df_test.iloc[:, 1:]).reshape([-1, 28, 28, 1])
+    # test_y = np.asarray(df_test.iloc[:, 0]).reshape([-1, 1])
 
-    return test_x, test_y
+    # return test_x, test_y
+
+    df = pd.read_csv(data_path, names=[
+                     "label"]+["pixel"+str(x) for x in range(784)])
+
+    X = np.asarray(df.iloc[:, 1:]).reshape([-1, 28, 28, 1])
+    y = np.asarray(df.iloc[:, 0]).reshape([-1, 1])
+
+    return X, y
 
 
 def setup(screen_size):
@@ -236,14 +262,28 @@ def setup(screen_size):
 
     # Loading and Visualizing Dataset
     # Loading emnist-balanced-mapping.txt
-    mapping = load_map("data/emnist-balanced-mapping.txt")
+    mapping_letters = load_map("data/emnist-letters-mapping.txt")
+    mapping_digits = load_map("data/emnist-digits-mapping.txt")
 
     # Loading testShort.csv
-    test_x, test_y = load_data("data/testShort.csv")
+    #test_x, test_y = load_data("data/testShort.csv")
+
+    # Loading training and testing data
+    test_x_letters, _ = load_data("data/emnist-letters_test.csv")
+    test_x_digits, _ = load_data("data/emnist-digits_test.csv")
 
     # Normalise Pixel Data
     # converting pixel values in range [0,1]
-    test_x = test_x/255
+    test_x_letters = test_x_letters/255
+    test_x_digits = test_x_digits/255
+
+    # Load model trained in line 1
+    # model = keras.models.load_model('data/model_e20_2')
+    model_letters = keras.models.load_model('data/model_letters_e20_2')
+    model_digits = keras.models.load_model('data/model_digits_e20_2')
+    # test_y = np.argmax(model.predict(test_x),axis =1)
+    test_y_letters = np.argmax(model_letters.predict(test_x_letters), axis=1)
+    test_y_digits = np.argmax(model_digits.predict(test_x_digits), axis=1)
 
     ### start predictive text code ###
     with open("data/web2.txt") as f:
@@ -269,22 +309,27 @@ def setup(screen_size):
     current_word = []
     sentence = []
 
-    digit = Digit(int((screen_x-200)/2), 20, 200, (0, 0, 0), screen, test_x, test_y, mapping)
+    digit = Digit(int((screen_x-200)/2), 20, 200, (0, 0, 0),
+                  screen, test_x_digits, mapping_digits)
 
-    new_img = Button(int((screen_x-150)/2), digit.y + digit.height + 20, 150, 50, 10, (0, 139, 139), "NEW IMAGE", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
-    select_img = Button(50, new_img.y + new_img.height + 20, 150, 50, 10, (0, 139, 139), "SELECT IMAGE", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
-    finish_word = Button(screen_x-150-50, new_img.y + new_img.height + 20, 150, 50, 10, (0, 139, 139), "FINISH WORD", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
+    new_img = Button(int((screen_x-150)/2), digit.y + digit.height + 20, 150, 50, 10, (0, 139, 139),
+                     "NEW IMAGE", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
+    select_img = Button(50, new_img.y + new_img.height + 20, 150, 50, 10, (0, 139, 139),
+                        "SELECT IMAGE", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
+    finish_word = Button(screen_x-150-50, new_img.y + new_img.height + 20, 150, 50, 10, (0, 139, 139),
+                         "FINISH WORD", pygame.font.Font('freesansbold.ttf', 32), (255, 255, 255), screen)
 
-    return done, screen, clock, current_word, sentence, digit, new_img, select_img, finish_word
+    return done, screen, clock, current_word, sentence, digit, new_img, select_img, finish_word, model_letters, model_digits, test_x_letters, test_y_letters, test_x_digits, test_y_digits, mapping_letters, mapping_digits
 
 
 def main(screen_size):
-    done, screen, clock, current_word, sentence, digit, new_img, select_img, finish_word = setup(screen_size)
+    done, screen, clock, current_word, sentence, digit, new_img, select_img, finish_word, model_letters, model_digits, test_x_letters, test_y_letters, test_x_digits, test_y_digits, mapping_letters, mapping_digits = setup(
+        screen_size)
 
     ### start main loop ###
 
     while not done:
-        letter = random.choice(string.ascii_lowercase)
+        # letter = random.choice(string.ascii_lowercase)
 
         screen.fill((255, 255, 255))
         # Blit everything to the screen
@@ -294,7 +339,8 @@ def main(screen_size):
         finish_word.draw()
 
         font = pygame.font.Font('freesansbold.ttf', 16)
-        text = font.render(" ".join(sentence) + " " + "".join(current_word), True, (0, 0, 0))
+        text = font.render(" ".join(sentence) + " " +
+                           "".join(current_word), True, (0, 0, 0))
         text_rect = text.get_rect()
         text_rect.center = (int(screen_size[0]/2), 400)
         screen.blit(text, text_rect)
@@ -305,8 +351,12 @@ def main(screen_size):
             elif event.type == pygame.MOUSEBUTTONUP:
                 mousex, mousey = event.pos
                 new_img.is_pressed(mousex, mousey, digit.gen_img)
-                select_img.is_pressed(mousex, mousey, append_letter, letter, current_word)
-                finish_word.is_pressed(mousex, mousey, append_word, current_word, sentence)
+                # select_img.is_pressed(mousex, mousey, append_letter, letter, current_word)
+                select_img.is_pressed(mousex, mousey, append_letter, digit.index,
+                                      current_word, model_letters, test_y_letters, mapping_letters)
+                # https://datascience.stackexchange.com/questions/13461/how-can-i-get-prediction-for-only-one-instance-in-keras
+                finish_word.is_pressed(
+                    mousex, mousey, append_word, current_word, sentence)
 
         pygame.display.flip()
         clock.tick(60)
